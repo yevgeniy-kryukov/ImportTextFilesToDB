3b7a9392494b374a02fa9bc7f52f443ed5516d77
yevgeniy.kryukov.1984 <yevgeniy.kryukov.1984@gmail.com>
2022-02-23 18:48:30 +0600

    disabled AutoCommit

--- src/main/java/org/ykryukov/import_text_files_to_db/ImportFileThread.java
+++ src/main/java/org/ykryukov/import_text_files_to_db/ImportFileThread.java
@@ -2,6 +2,8 @@ package org.ykryukov.import_text_files_to_db;
 
 import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class ImportFileThread extends Thread {
   
@@ -21,15 +23,28 @@ public class ImportFileThread extends Thread {
   public void run() {
     try {
       try (final Connection conn = ConnDB.getConnection()) {
+		conn.setAutoCommit(false);
         final ReadFile readFile = new ReadFile(this.filePath);
         final ArrayList<String> fieldNames = readFile.getNextLineAsArrayList(this.delimiter);
         if (fieldNames != null) {
           final WriteDB writeDB = new WriteDB(conn, this.tableName);
           if (writeDB.isColumnsNamesCorrect(fieldNames)) {
             ArrayList<String> fieldValues;
-            while ((fieldValues = readFile.getNextLineAsArrayList(this.delimiter)) != null) {
-              writeDB.writeRow(fieldNames, fieldValues);
-            }
+			int rowsCount = 0;
+			try {
+				while ((fieldValues = readFile.getNextLineAsArrayList(this.delimiter)) != null) {
+				  writeDB.writeRow(fieldNames, fieldValues);
+				  rowsCount++;
+				  if (rowsCount > 999) {
+					  conn.commit();
+					  rowsCount = 0;
+				  }
+				}
+				conn.commit();
+			} catch(Exception ex) {
+				conn.rollback();
+				throw ex;
+			}
           } else {
             System.out.println("File field name is not correct or field name is not exists!");
           }
@@ -37,7 +52,7 @@ public class ImportFileThread extends Thread {
           System.out.println("File is empty!");
         }
       }
-      System.out.println("Finished import '" + this.filePath + "'");
+      System.out.println("Finished import '" + this.filePath + "' at " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date()));
     } catch(Exception ex) {
       System.out.println(ex);
     }  
