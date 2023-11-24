849409f69b7949521dacb88356a091407cf35e02
yevgeniy.kryukov.1984 <yevgeniy.kryukov.1984@gmail.com>
2023-11-25 00:08:01 +0600

    2023-11-25 refactoring

--- src/main/java/org/ykryukov/import_text_files_to_db/WriteDB.java
+++ src/main/java/org/ykryukov/import_text_files_to_db/WriteDB.java
@@ -15,18 +15,18 @@ class WriteDB {
     final private ArrayList<String> columnsNamesList;
     final private HashMap<String, String> columnsTypes;
 
-    WriteDB(final Connection connenction, final String tableName) throws SQLException {
-        conn = connenction;
-        tbName = tableName;
-        final String query = "SELECT * FROM " + tbName + " LIMIT 1";
-        try (final Statement statement = conn.createStatement();
+    WriteDB(final Connection connection, final String tableName) throws SQLException {
+        this.conn = connection;
+        this.tbName = tableName;
+        final String query = "SELECT * FROM " + this.tbName + " LIMIT 1";
+        try (final Statement statement = this.conn.createStatement();
              final ResultSet resultSet = statement.executeQuery(query)) {
             final ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
-            columnsNamesList = new ArrayList<String>();
-            columnsTypes = new HashMap<String, String>();
+            this.columnsNamesList = new ArrayList<String>();
+            this.columnsTypes = new HashMap<String, String>();
             for (int i = 1; i <= resultSetMetaData.getColumnCount(); i++) {
-                columnsTypes.put(resultSetMetaData.getColumnName(i), resultSetMetaData.getColumnTypeName(i));
-                columnsNamesList.add(resultSetMetaData.getColumnName(i));
+                this.columnsTypes.put(resultSetMetaData.getColumnName(i), resultSetMetaData.getColumnTypeName(i));
+                this.columnsNamesList.add(resultSetMetaData.getColumnName(i));
             }
         }
     }
@@ -35,7 +35,7 @@ class WriteDB {
         boolean found;
         for (String itemColumnNameFile : columnsNamesListFile) {
             found = false;
-            for (String itemColumnName : columnsNamesList) {
+            for (String itemColumnName : this.columnsNamesList) {
                 if (itemColumnNameFile.equals(itemColumnName)) {
                     found = true;
                     break;
@@ -47,38 +47,38 @@ class WriteDB {
     }
 
     private String getColumnTypeByName(final String columnName) {
-        return columnsTypes.get(columnName);
+        return this.columnsTypes.get(columnName);
     }
 
     private void setPrepareStatementValues(PreparedStatement preparedStatement,
                                            final ArrayList<String> fieldNames,
                                            final ArrayList<String> fieldValues) throws SQLException {
         int i = 0;
-        String typeName = "";
         for (String item : fieldValues) {
             i++;
-            typeName = getColumnTypeByName(fieldNames.get(i - 1));
-            if (typeName == "int8") {
-                preparedStatement.setLong(i, Long.parseLong(item));
-            } else if (typeName == "varchar") {
-                preparedStatement.setString(i, item);
-            } else if (typeName == "text") {
-                preparedStatement.setString(i, item);
+            switch (getColumnTypeByName(fieldNames.get(i - 1))) {
+                case "int8":
+                    preparedStatement.setLong(i, Long.parseLong(item));
+                    break;
+                case "varchar":
+                case "text":
+                    preparedStatement.setString(i, item);
+                    break;
             }
         }
     }
 
     void writeRow(final ArrayList<String> fieldNames, final ArrayList<String> fieldValues) throws SQLException {
-        String sqlFieldNames = "";
-        String sqlFieldMaskValues = "";
+        StringBuilder sqlFieldNames = new StringBuilder();
+        StringBuilder sqlFieldMaskValues = new StringBuilder();
         String sqlDelimeter = "";
         for (String item : fieldNames) {
-            sqlFieldNames = sqlFieldNames + sqlDelimeter + item;
-            sqlFieldMaskValues = sqlFieldMaskValues + sqlDelimeter + "?";
+            sqlFieldNames.append(sqlDelimeter).append(item);
+            sqlFieldMaskValues.append(sqlDelimeter).append("?");
             sqlDelimeter = ",";
         }
-        final String sqlText = "INSERT INTO " + tbName + " (" + sqlFieldNames + ") VALUES (" + sqlFieldMaskValues + ")";
-        try (final PreparedStatement preparedStatement = conn.prepareStatement(sqlText)) {
+        final String sqlText = "INSERT INTO " + this.tbName + " (" + sqlFieldNames + ") VALUES (" + sqlFieldMaskValues + ")";
+        try (final PreparedStatement preparedStatement = this.conn.prepareStatement(sqlText)) {
             setPrepareStatementValues(preparedStatement, fieldNames, fieldValues);
             final int rowsCount = preparedStatement.executeUpdate();
         }
