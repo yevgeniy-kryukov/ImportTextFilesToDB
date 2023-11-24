9e6f860adf62af6420d236f7c3eadf8f3c8f1c2c
yevgeniy.kryukov.1984 <yevgeniy.kryukov.1984@gmail.com>
2023-11-25 00:15:29 +0600

    2023-11-25 refactoring

--- src/main/java/org/ykryukov/import_text_files_to_db/ConnDB.java
+++ src/main/java/org/ykryukov/import_text_files_to_db/ConnDB.java
@@ -2,7 +2,6 @@ package org.ykryukov.import_text_files_to_db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.io.InputStream;
 import java.util.Properties;
 
 class ConnDB {
@@ -12,10 +11,8 @@ class ConnDB {
     static Connection getConnection() throws Exception {
         Class.forName("org.postgresql.Driver").getDeclaredConstructor().newInstance();
         final Properties props = ResourceFileUtil.getFileProperties("app.properties");
-        final String url = props.getProperty("url");
-        final String username = props.getProperty("username");
-        final String password = props.getProperty("password");
-
-        return DriverManager.getConnection(url, username, password);
+        return DriverManager.getConnection(props.getProperty("url"),
+                props.getProperty("username"),
+                props.getProperty("password"));
     }
 }
