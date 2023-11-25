c80adcc8aeaa245884b525fe70be2ac45e669624
yevgeniy.kryukov.1984 <yevgeniy.kryukov.1984@gmail.com>
2023-11-25 18:00:01 +0600

    2023-11-26 refactoring

--- src/main/java/org/ykryukov/import_text_files_to_db/ImportFileThread.java
+++ src/main/java/org/ykryukov/import_text_files_to_db/ImportFileThread.java
@@ -26,36 +26,36 @@ class ImportFileThread extends Thread {
                 conn.setAutoCommit(false);
                 final ReadFile readFile = new ReadFile(this.filePath);
                 final ArrayList<String> fieldNames = readFile.getNextLineAsArrayList(this.delimiter);
-                if (fieldNames != null) {
-                    final WriteDB writeDB = new WriteDB(conn, this.tableName);
-                    if (writeDB.isColumnsNamesCorrect(fieldNames)) {
-                        ArrayList<String> fieldValues;
-                        int rowsCount = 0;
-                        try {
-                            while ((fieldValues = readFile.getNextLineAsArrayList(this.delimiter)) != null) {
-                                writeDB.writeRow(fieldNames, fieldValues);
-                                rowsCount++;
-                                if (rowsCount > 999) {
-                                    conn.commit();
-                                    rowsCount = 0;
-                                }
-                            }
-                            conn.commit();
-                        } catch (Exception ex) {
-                            conn.rollback();
-                            throw ex;
-                        }
-                    } else {
-                        System.out.println("File field name is not correct or field name is not exists!");
-                    }
-                } else {
+                if (fieldNames == null) {
                     System.out.println("File is empty!");
+                    return;
+                }
+                final WriteDB writeDB = new WriteDB(conn, this.tableName);
+                if (!writeDB.isColumnsNamesCorrect(fieldNames)) {
+                    System.out.println("File field name is not correct or field name is not exists!");
+                    return;
+                }
+                ArrayList<String> fieldValues;
+                int rowsCount = 0;
+                try {
+                    while ((fieldValues = readFile.getNextLineAsArrayList(this.delimiter)) != null) {
+                        writeDB.writeRow(fieldNames, fieldValues);
+                        rowsCount++;
+                        if (rowsCount > 999) {
+                            conn.commit();
+                            rowsCount = 0;
+                        }
+                    }
+                    conn.commit();
+                } catch (Exception ex) {
+                    conn.rollback();
+                    throw ex;
                 }
             }
             System.out.println("Finished import '" + this.filePath
                     + "' at " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date()));
         } catch (Exception ex) {
-            System.out.println(ex);
+            System.out.println(ex.getMessage());
         }
     }
 
--- src/main/java/org/ykryukov/import_text_files_to_db/Main.java
+++ src/main/java/org/ykryukov/import_text_files_to_db/Main.java
@@ -1,6 +1,5 @@
 package org.ykryukov.import_text_files_to_db;
 
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Date;
 import java.text.SimpleDateFormat;
@@ -8,33 +7,25 @@ import java.util.Properties;
 
 class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Started at " + new java.util.Date());
 
         ArrayList<Thread> listThreads = new ArrayList<Thread>();
 
-        try {
-            final Properties properties = ResourceFileUtil.getFileProperties("app.properties");
-            final ReadFile listFiles = new ReadFile(properties.getProperty("configFilePath"));
-            int i = 0;
-            ArrayList<String> line;
-            while ((line = listFiles.getNextLineAsArrayList(";")) != null) {
-                i++;
-                Thread t = new ImportFileThread("ImportFileThread" + i, line.get(0), line.get(1), line.get(2));
-                t.start();
-                listThreads.add(t);
-                System.out.println("Started import '" + line.get(0) + "'");
-            }
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+        final Properties properties = ResourceFileUtil.getFileProperties("app.properties");
+        final ReadFile listFiles = new ReadFile(properties.getProperty("configFilePath"));
+        int i = 0;
+        ArrayList<String> line;
+        while ((line = listFiles.getNextLineAsArrayList(";")) != null) {
+            i++;
+            Thread t = new ImportFileThread("ImportFileThread" + i, line.get(0), line.get(1), line.get(2));
+            t.start();
+            listThreads.add(t);
+            System.out.println("Started import '" + line.get(0) + "'");
         }
 
         for (Thread item : listThreads) {
-            try {
-                item.join();
-            } catch (InterruptedException e) {
-                System.out.printf("%s has been interrupted", item.getName());
-            }
+            item.join();
         }
 
         System.out.println("Finished at " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date()));
