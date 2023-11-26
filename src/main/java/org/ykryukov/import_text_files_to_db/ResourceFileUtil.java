83929a30575c997d003d9ba2528728c77629ff35
yevgeniy.kryukov.1984 <yevgeniy.kryukov.1984@gmail.com>
2023-11-26 14:44:02 +0600

    2023-11-26 refactoring

--- src/main/java/org/ykryukov/import_text_files_to_db/ResourceFileUtil.java
+++ src/main/java/org/ykryukov/import_text_files_to_db/ResourceFileUtil.java
@@ -2,30 +2,27 @@ package org.ykryukov.import_text_files_to_db;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 class ResourceFileUtil {
-    public static Properties getFileProperties(final String fileName) {
+    public static Properties getFileProperties(final String fileName) throws IOException {
         // устанавливаем свойства из файла в ресурсах
         final Properties props = new Properties();
-        try (InputStream in = ResourceFileUtil.class.getClassLoader().getResourceAsStream(fileName)) {
+        try (final InputStream in = ResourceFileUtil.class.getClassLoader().getResourceAsStream(fileName)) {
             if (in == null) {
                 throw new IllegalArgumentException("file 'app.properties' not found!");
             }
             props.load(in);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
         }
 
         // перезаписываем свойства из файла в рабочей директории
         final Properties propsOver = new Properties();
         String userDirectory = System.getProperty("user.dir");
-        try (InputStream in = new FileInputStream(userDirectory + "/" + fileName)) {
+        try (final InputStream in = new FileInputStream(userDirectory + "/" + fileName)) {
             propsOver.load(in);
         } catch (FileNotFoundException ignored) {
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
         }
         for (String key : propsOver.stringPropertyNames()) {
             props.setProperty(key, propsOver.getProperty(key));
